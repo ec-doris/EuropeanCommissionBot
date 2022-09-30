01a2f16d95b387e3bed69f4bf20ed2a215771fb2
J.M <jonathan.mallet3872@gmail.com>
2022-09-30 17:14:21 +0200

    Adding limit number of edit

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -19,30 +19,36 @@ def sparql_query(sparql: SPARQLWrapper, query: str, print_query: bool = False) -
     return sparql.query().convert()
 
 
-def remove_duplicate(wikibase_repo):
+def remove_duplicate(wikibase_repo, edit_limit: int = 0):
     query = '''SELECT ?s WHERE { ?s wdt:P11012 ?o. }'''
     sparql = SPARQLWrapper('https://query.wikidata.org/sparql')
     res = sparql_query(sparql, query)['results']['bindings']
+    nedit = 0
     for r in res:
-        qid = r['s']['value'].replace('http://www.wikidata.org/entity/', '')
-        wikidata_item = pywikibot.ItemPage(wikibase_repo, qid)
-        wikidata_item.get()
-        qids = set()
-        claims_to_remove = []
-        if 'P11012' in wikidata_item.claims:
-            for claim in wikidata_item.claims['P11012']:
-                if claim.getTarget() not in qids:
-                    qids.add(claim.getTarget())
-                else:
-                    print(f'http://www.wikidata.org/entity/{qid}', 'has duplicate', claim.getTarget(), 'removing it.')
-                    claims_to_remove.append(claim)
+        if nedit < edit_limit or edit_limit <= 0:
+            qid = r['s']['value'].replace('http://www.wikidata.org/entity/', '')
+            wikidata_item = pywikibot.ItemPage(wikibase_repo, qid)
+            wikidata_item.get()
+            qids = set()
+            claims_to_remove = []
+            if 'P11012' in wikidata_item.claims:
+                for claim in wikidata_item.claims['P11012']:
+                    if claim.getTarget() not in qids:
+                        qids.add(claim.getTarget())
+                    else:
+                        print(f'\thttp://www.wikidata.org/entity/{qid}', 'has duplicate', claim.getTarget())
+                        claims_to_remove.append(claim)
 
-        if len(claims_to_remove) > 0:
-            wikidata_item.removeClaims(claims_to_remove, summary='Removing duplicate')
+            if len(claims_to_remove) > 0:
+                print(f'http://www.wikidata.org/entity/{qid}', 'removing', len(claims_to_remove), 'claims.')
+                wikidata_item.removeClaims(claims_to_remove, summary='Removing duplicate')
+                nedit += 1
+            else:
+                print(f'http://www.wikidata.org/entity/{qid}', 'has no duplicate')
 
 
 if __name__ == '__main__':
     wikibase = pywikibot.Site('wikidata', 'wikidata')
     wikibase_repo = wikibase.data_repository()
     wikibase_repo.login()
-    remove_duplicate(wikibase_repo)
+    remove_duplicate(wikibase_repo, 3)
